bef11af06fd2d5ecd5c7216fd80766eba6fb5b8e
michael berezin <mbearz@gmail.com>
2018-07-15 19:59:45 +0300

    MinimalToturial

---
+++ GrainCollection/HelloGrain.cs
@@ -0,0 +1,36 @@
+﻿using GrainInterfaces;
+using System.Threading.Tasks;
+using System;
+
+namespace GrainCollection
+{
+    class HelloGrain : Orleans.Grain, IHello
+    {
+        private readonly string Key = "key1";
+        public Task<string> SayHello(string msg)
+        {
+            return Task.FromResult(string.Format("You said {0}, I say: Hello!", msg));
+        }
+
+        public Task<string> SayHelloInReverse(string text)
+        {
+            var r = ReverseString(text);
+            //RequestContext.Set(Key, r);
+            return Task.FromResult($"Reverse: " + r);
+        }
+
+        public Task<string> FromContext()
+        {
+           // var val = RequestContext.Get(Key) ?? "No Key";
+
+            return Task.FromResult(Key);
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
---
+++ GrainInterfaces/IHello.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace GrainInterfaces
+{
+    public interface IHello : Orleans.IGrainWithIntegerKey
+    {
+        Task<string> SayHello(string msg);
+
+        Task<string> SayHelloInReverse(string texts);
+
+        Task<string> FromContext();
+    }
+}
